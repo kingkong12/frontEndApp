fe53cf75be1a088e108bd6517f416be54be247bc
vasanimithil999@gmail.com <vasanimithil999@gmail.com>
2020-04-19 21:13:03 +0530

    pagination wip

--- src/ui/templates/Dashboard.js
+++ src/ui/templates/Dashboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/forbid-prop-types */
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
@@ -6,14 +7,37 @@ import udpateListItems from 'actions/dashboardAction'
 import Card from 'ui/organisms/Card'
 import propTypes from 'prop-types'
 import { push } from 'connected-react-router'
+import { SimplePagination } from 'react-swag-pagination'
+
+const recordPerPage = 10
 
 class Dasbaord extends Component {
-  componentDidMount() {
+  constructor() {
+    super()
+    this.state = {
+      currentPage: 2,
+      steps: 0,
+      listArray: []
+    }
+  }
+
+  onSuccessFetch = (res) => {
+    // conditoin whre record is less then 10
     const { updateReduxList } = this.props
+    let steps = res.data.resultCount / 10 || 1
+    let lastindex = this.state.currentPage * recordPerPage || 0
+    let firstIndex = lastindex - recordPerPage || 0
+    let listArray = res.data.results.slice(firstIndex, lastindex)
+    this.setState({ steps, listArray })
+    // redux dispatch
+    updateReduxList(res.data)
+  }
+
+  componentDidMount() {
     const api = 'https://itunes.apple.com/search?term=ColdPlay'
     axios
       .get(api)
-      .then((res) => updateReduxList(res.data))
+      .then((res) => this.onSuccessFetch(res))
       .catch((error) => console.log('error', error))
   }
 
@@ -25,13 +49,42 @@ class Dasbaord extends Component {
     })
   }
 
+  nav = (currentPage) => this.setState({ currentPage })
+  next = () => this.setState({ currentPage: this.state.currentPage + 1 })
+  last = () => this.setState({ currentPage: this.state.steps })
+  first = () => this.setState({ currentPage: 1 })
+  back = () => this.setState({ currentPage: this.state.currentPage - 1 })
+
+  renderEmptyCOntianer = () => {
+    console.log('conatiner')
+
+    return (
+      <EmptyWrapper>
+        <div> No records Found </div>
+      </EmptyWrapper>
+    )
+  }
+
   render() {
+    const { currentPage, steps } = this.state
     const { dashboardList } = this.props
+
+    console.log('props . . .', this.props)
     return (
       <Container>
         <Heading> Tracks of Coldplay </Heading>
+        <PaginationWrapper>
+          <SimplePagination
+            steps={steps}
+            defaultStep={1}
+            callbackNav={this.nav}
+            callbackNext={this.next}
+            callbackPrev={this.back}
+            font="Roboto"
+          />
+        </PaginationWrapper>
         <CardWrapper>
-          {dashboardList.list.map((item) => (
+          {this.state.listArray.map((item) => (
             <Card
               key={item.trackId}
               item={item}
@@ -83,4 +136,16 @@ const CardWrapper = styled.div`
   flex-flow: row wrap;
   justify-content: center;
   width: 100%;
+`
+const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 1.5rem 2rem 0 2rem;
+`
+
+const EmptyWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: 'center';
+  align-item: 'center';
 `
